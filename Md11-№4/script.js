af5330063627f9666d4e5476c834e957d07def97
Slava <118695555+Veaceslava@users.noreply.github.com>
2022-12-24 16:05:54 +0200

    Add files via upload

---
+++ Md11-№4/script.js
@@ -0,0 +1,21 @@
+"use stirct";
+
+/* Модудь 11*/
+
+
+/* Модудь 11 / Задание 4 */
+
+
+function printNumbers(from, to) {
+    let current = from;
+  
+    let timerId = setInterval(function() {
+      alert(current);
+      if (current == to) {
+        clearInterval(timerId);
+      }
+      current++;
+    }, 1000);
+  }
+  
+  printNumbers(5, 10);
