b222ac70cc0b74b317736e888dc37f23791d69f1
Slava <118695555+Veaceslava@users.noreply.github.com>
2022-12-24 16:05:14 +0200

    Add files via upload

---
+++ Md11-№1/script.js
@@ -0,0 +1,22 @@
+"use stirct";
+
+/* Модудь 11*/
+
+
+/* Модудь 11 / Задание 1 */
+
+let arr = [2, 4, 6, 8, 3, 5, 7];
+let even = 0;
+let odd = 0;
+for (let key of arr) {
+
+  if (key % 2 == 0) {
+    console.log('Четное:', key);
+    even++;
+  } else if (key % 2 ==1) {
+    console.log('Нечетное:', key);
+    odd++;
+  }
+}
+console.log('Количество четных чисел:', even);
+console.log('Количество нечетных чисел:', odd);
